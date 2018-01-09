94b2aab0fbc2f412a32e3fa67d3965f9c56af47e
yewenpo <ityewenpo@163.com>
2018-01-09 18:30:50 +0800

    解决border1px问题

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
       </div>
@@ -22,6 +22,11 @@
   import goods from './components/goods/goods.vue';
   export default {
     name: 'app',
+    data () {
+      return {
+
+      };
+    },
     components: {
       'v-header': Header,
       'goods': goods
@@ -30,12 +35,14 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "common/stylus/mixin.styl"
   #app
     .tab
       display:flex
       width: 100%
       height: 40px
       line-height: 40px
+      border-1px(rgba(7,17,27,0.1))
       .tab-item
         flex:1
         text-align :center
